ecf306f8472bc4dd6f20be465fac02c4ac5f16e1
Dan Kozlowski <dakozlowski@gmail.com>
2017-07-22 11:32:57 -0700

    Fix the keyboard

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-sudo apt-get install i3 rofi nitrogen i3blocks terminator tmux git screen
+sudo apt-get install i3 rofi nitrogen i3blocks terminator tmux git screen silversearcher-ag jq golang-go gnome-tweak-tool
+
+# Ruby reqs
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+
